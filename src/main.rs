fb083ee0344c8845ac9b3be2831006b61e462db3
akash <akash2320@gmail.com>
2025-11-18 11:27:07 -0500

    added animation support

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@
 mod types;
 mod data_loaders;
 mod plotting;
-
+use serde_json::{Value, json};
 use polars::prelude::*;
 use polars::datatypes::Float64Type;
-use ndarray::{Array1, s};
-use indexmap::IndexMap;
+use ndarray::{Array1,Array2, s};
+use plotly::{Plot, Scatter, layout, common::*, plot::Trace, plot::Traces};
+use plotly::color::{Color};
+use plotly::layout::{Axis, Layout, Legend, Slider, SliderStep, Frame, SliderMethod, SliderCurrentValue};
+use plotly::layout::update_menu::{Button, ButtonMethod, UpdateMenu, UpdateMenuDirection, UpdateMenuType};
 
 use types::*;
 use data_loaders::*;
@@ -15,14 +18,16 @@ use plotting::*;
 
 fn main() {
     // LOADING LOGIC
-    let rotation = load_rotation("rotation.json");
+    let rotation = load_rotation("data/rotation.json");
     let track_angle =  (rotation / 180.0) * std::f64::consts::PI;
-    let positional_data: PositionalData = load_positional_data("positional_data_dict.json");
-    let fastest_lap_data = load_fastest_lap_data("fastest_lap_pos.json");
-    let abv = load_abbrevations("abv.json");
+    let positional_data: PositionalData = load_positional_data("data/positional_data_dict.json");
+    let fastest_lap_data = load_fastest_lap_data("data/fastest_lap_pos.json");
+    let abv = load_abbrevations("data/abv.json");
+    let team_colors = load_team_colors("data/team_colors.json");
+    let winner_abv = load_winner_abv("data/winner.json");
     // println!("Loaded positional data for {}", positional_data.get(abv.get("VER").unwrap()).unwrap());
 
-    let x_series = positional_data.get(abv.get("VER").unwrap()).unwrap().column("X").unwrap();
+    let x_series = positional_data.get(abv.get(winner_abv.as_str()).unwrap()).unwrap().column("X").unwrap();
     let x_vec: Vec<f64> = x_series.f64().unwrap().into_no_null_iter().collect();
     let x_ndarray = Array1::from(x_vec);
     let min_flat = 100;
@@ -84,14 +89,134 @@ fn main() {
 
     let xy = df_with_pos.select(["X", "Y"]).unwrap().to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
     
+    let frames_vec: Vec<i32> = df_with_pos.column("Frame").unwrap().unique().unwrap().sort(SortOptions::default()).unwrap().i32().unwrap().into_no_null_iter().collect();
+    // let frames_vec: Vec<i32> = (0..5000).collect();
     let rotated = rotate2d(xy, track_angle);  // or track_angle if in radians
     df_with_pos.replace("X", Series::new("X".into(), rotated.slice(s![..,0]).to_vec())).unwrap();
     df_with_pos.replace("Y", Series::new("Y".into(), rotated.slice(s![..,1]).to_vec())).unwrap();
-    df_with_pos = df_with_pos.lazy().filter(col("Frame").lt(lit(1000))).collect().unwrap();
+    // df_with_pos = df_with_pos.lazy().filter(col("Frame").lt(lit(5000))).collect().unwrap();
 
-    println!("Loaded positional data for {:?}", df_with_pos);
+    // println!("Loaded positional data for {:?}", df_with_pos);
     
     let track = fastest_lap_data.to_ndarray::<Float64Type>(IndexOrder::Fortran).unwrap();
     let rotated_track = rotate2d(track, track_angle);
-    plot_track(rotated_track);
+    // plot_track(rotated_track);
+    let mut plot = Plot::new();
+    let mut track_trace = Scatter::new(rotated_track.slice(s![.., 0]).to_vec(),
+                                                     rotated_track.slice(s![.., 1]).to_vec());
+    let line_trace = Line::new().width(0.9).color("red");
+    track_trace = track_trace.show_legend(false)
+                .hover_info(HoverInfo::None)
+                .line(line_trace);
+    let cloned_track_trace = track_trace.clone();
+
+
+    let mut layout = Layout::new();
+    layout = layout.x_axis(layout::Axis::new().visible(false))
+                    .y_axis(layout::Axis::new().visible(false))
+                    .title("Live Grand Prix")
+                    .legend(Legend::new().title("Driver"));
+    plot.add_trace(track_trace);
+    
+    // plot.show();
+    
+
+
+    // let frames_vec: Vec<i32> = df_with_pos.column("Frame").unwrap().unique().unwrap().sort(SortOptions::default()).unwrap().i32().unwrap().into_no_null_iter().collect();
+    
+    let min_frame = *frames_vec.first().unwrap_or(&0);
+
+    let mut base_traces: Vec<Box<dyn Trace>> = Vec::new();
+    for driver in abv.keys() {
+        let filtered = df_with_pos.clone().lazy()
+            .filter(col("Frame").eq(lit(min_frame)).and(col("Driver").eq(lit(driver.as_str()))))
+            .select(&[col("X"), col("Y")])
+            .collect().unwrap();
+        let x: Vec<f64> = filtered.column("X").unwrap().f64().unwrap().into_no_null_iter().collect();
+        let y: Vec<f64> = filtered.column("Y").unwrap().f64().unwrap().into_no_null_iter().collect();
+
+        let trace: Box<dyn Trace> = Scatter::new(x, y)
+            .mode(Mode::Markers)
+            .text_array(vec![driver.clone()])
+            .marker(Marker::new().color(team_colors.get(driver).unwrap().clone()))
+            .name(driver.clone())
+            .legend_group(driver.clone())
+            .show_legend(true)
+            .ids(vec![driver.clone()]);
+        base_traces.push(trace);
+    }
+    plot.add_traces(base_traces);
+
+    for &frame_val in &frames_vec {
+        let mut frame_traces: Traces = Traces::new();
+        frame_traces.push(cloned_track_trace.clone());
+        for driver in abv.keys() {
+            let filtered = df_with_pos.clone().lazy()
+                .filter(col("Frame").eq(lit(frame_val)).and(col("Driver").eq(lit(driver.as_str()))))
+                .select(&[col("X"), col("Y")])
+                .collect().unwrap();
+            let x: Vec<f64> = filtered.column("X").unwrap().f64().unwrap().into_no_null_iter().collect();
+            let y: Vec<f64> = filtered.column("Y").unwrap().f64().unwrap().into_no_null_iter().collect();
+
+            let trace: Box<dyn Trace> = Scatter::new(x, y)
+                    .mode(Mode::Markers)
+                    .text_array(vec![driver.clone()])
+                    .marker(Marker::new().color(team_colors.get(driver).unwrap().clone()))
+                    .name(driver.clone())
+                    .legend_group(driver.clone())
+                    .show_legend(true)
+                    .ids(vec![driver.clone()]);
+            frame_traces.push(trace);
+        }
+        let frame = Frame::new()
+            .name(frame_val.to_string())
+            .data(frame_traces);  // Adds the traces to the frame's data.
+        plot.add_frame(frame);
+    }
+
+    let play_button = Button::new()
+        .label("▶")
+        .method(ButtonMethod::Animate)
+        .args(json!([null,{"frame": {"duration": 10, "redraw": false}, "mode": "next", "fromcurrent": true, "transition": {"duration": 2, "easing": "linear"}}]));
+    let pause_button = Button::new()
+        .label("⏸")
+        .method(ButtonMethod::Animate)
+        .args(json!([[null],{"frame": {"duration": 0, "redraw": false}, "mode": "immediate", "fromcurrent": true, "transition": {"duration": 0, "easing": "linear"}}]));
+    let update_menu = UpdateMenu::new()
+        .buttons(vec![play_button, pause_button])
+        .direction(UpdateMenuDirection::Left)
+        .pad(Pad::new(70, 0, 10))
+        .show_active(false)
+        .ty(UpdateMenuType::Buttons)
+        .x(0.1)
+        .x_anchor(Anchor::Right)
+        .y(0.0)
+        .y_anchor(Anchor::Top);
+    layout = layout.update_menus(vec![update_menu]);
+
+    let mut slider_steps: Vec<SliderStep> = Vec::new();
+    for &frame_val in &frames_vec {
+        let step = SliderStep::new()
+            .args(json!([[frame_val.to_string()],{"frame": {"duration": 0, "redraw": false}, "mode": "immediate", "fromcurrent": true, "transition": {"duration": 0, "easing": "linear"}}]))
+            .label(frame_val.to_string())
+            .method(SliderMethod::Animate);
+        slider_steps.push(step);
+    }
+    let slider = Slider::new()
+        .active(0)
+        .current_value(SliderCurrentValue::new().prefix("Frame="))
+        .length(0.9)
+        .pad(Pad::new(60,10,0))
+        .steps(slider_steps)
+        .x(0.1)
+        .x_anchor(Anchor::Left)
+        .y(0.0)
+        .y_anchor(Anchor::Top);
+    layout = layout.sliders(vec![slider]);
+
+    plot.set_layout(layout);
+    // plot.show();
+    plot.write_html("track.html");
+    
+
 }
